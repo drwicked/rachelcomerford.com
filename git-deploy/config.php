61b2f797ebef0989c173cb76c5c7c8e809234521
Jeff Printy <jeff.printy.contractor@macmillan.com>
2017-09-28 17:48:42 -0500

    testing git deploy

--- git-deploy/config.php
+++ git-deploy/config.php
@@ -1,6 +1,6 @@
 <?php
 define("TOKEN", "drofremoclehcar");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
-define("REMOTE_REPOSITORY", "git@github.com/drwicked/rachelcomerford.com"); // The SSH URL to your repository
+define("REMOTE_REPOSITORY", "git@github.com:drwicked/rachelcomerford.com.git"); // The SSH URL to your repository
 define("DIR", "/var/www/comerford/");                          // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/gh-pages");                                 // The branch route
 define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
